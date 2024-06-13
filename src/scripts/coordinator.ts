36db99d80191575b2b209ce08a65d203c6191521
Thomas Sobieck <thomas@sobieck.com>
2024-06-13 09:04:18 -0500

    Cleaning up data folder when starting coor

--- src/scripts/coordinator.ts
+++ src/scripts/coordinator.ts
@@ -2,16 +2,20 @@ import { NS } from "@ns";
 import { ScriptRegistry } from "/scripts/scriptRegistry"
 
 export async function main(ns: NS): Promise<void> {
-  ns.run("scripts/scriptRegistry.js")
-
-// feed param to clean up data
 
   const totalTimeInMs = 2_000
 
+  if (ns.args[0] === "clean") {
+    ns.run("scripts/utilities/dataClean.js")
+    await ns.sleep(totalTimeInMs * 3)
+  }
+
+  ns.run("scripts/scriptRegistry.js")
+
   await ns.sleep(totalTimeInMs * 3)
 
-  while(true){
-    
+  while (true) {
+
     const scriptRegistry: ScriptRegistry = JSON.parse(ns.read("data/scriptRegistry.txt"))
     const numberOfMsPerAction = totalTimeInMs / scriptRegistry.scriptsWithCost.length
 
@@ -19,7 +23,7 @@ export async function main(ns: NS): Promise<void> {
 
       ns.run(script)
       await ns.sleep(numberOfMsPerAction)
-    
+
     } 
-  }
+  } 
 }
